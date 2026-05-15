a2ec17327a321f03c131bec03e61585e327d14d9
Panthini Patel <panthinipatel5@gmail.com>
2026-05-15 17:01:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,81 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import joblib
+#import libraries
+import pandas as pd #for file read and data manipulation
+import numpy as np #for numeric calculation
+import matplotlib.pyplot as plt #for data visualization
+import seaborn as sns #for advance data visualization
+import streamlit as st #for interactive dashboard
+import joblib #for loading the model
 
+#tab settings
 st.set_page_config(
-    page_title="Iris Flower Dashboard",
-    page_icon="🌸",
-    layout="wide"
-)
+    page_title = 'Iris Flower Dashboard', 
+    page_icon = '🌸', 
+    layout = 'wide')
 
-df = pd.read_csv("Iris.csv")
+#load dataset
+df = pd.read_csv('Iris.csv')
 
-df = df.drop("Id", axis=1)
+#creating a copy of dataset
+df_copy = df.copy()
 
-model, le = joblib.load("iris_model.pkl")
+#removing unwanted columns
+df_copy.drop('Id', axis = 1)
 
-st.title("🌸 Iris Flower Classification Dashboard")
+#load the model
+model, le = joblib.load('iris_model.pkl')
 
+#header
+
+#dashboard title
+st.title('🌸 Iris Flower Classification Dashboard')
+
+#description
 st.write("""
-This dashboard predicts the species of an Iris flower
-using Machine Learning.
+This dashboard predicts the species of an Iris Flower using Machine Learning
 """)
 
-st.sidebar.header("Enter Flower Measurements")
+#horizontal line
+st.markdown('---')
 
+#sidebar creation
+
+#header
+st.sidebar.header('Enter Flower Measurements')
+
+#numer input fields
 sepal_length = st.sidebar.number_input(
-    "Sepal Length",
-    min_value=0.0,
-    format="%.2f"
-)
+    'Sepal Length', 
+    min_value = 0.0, 
+    format = '%.2f')
 
 sepal_width = st.sidebar.number_input(
-    "Sepal Width",
-    min_value=0.0,
-    format="%.2f"
-)
+    'Sepal Width',
+    min_value = 0.0,
+    format = '%.2f')
 
 petal_length = st.sidebar.number_input(
-    "Petal Length",
-    min_value=0.0,
-    format="%.2f"
-)
+    'Petal Length',
+    min_value = 0.0,
+    format = '%.2f')
 
 petal_width = st.sidebar.number_input(
-    "Petal Width",
-    min_value=0.0,
-    format="%.2f"
-)
+    'Petal Width',
+    min_value = 0.0,
+    format = '%.2f')
 
-if st.sidebar.button("Predict"):
-
+#predict button
+if st.sidebar.button('Predict'):
     input_data = pd.DataFrame(
-        [[
-            sepal_length,
-            sepal_width,
-            petal_length,
-            petal_width
-        ]],
+        [[sepal_length, 
+          sepal_width, 
+          petal_length,
+          petal_width]],
 
-        columns=[
-            "SepalLengthCm",
-            "SepalWidthCm",
-            "PetalLengthCm",
-            "PetalWidthCm"
+        columns = [
+            'SepalLengthCm',
+            'SepalWidthCm',
+            'PetalLengthCm',
+            'PetalWidthCm'
         ]
     )
 
@@ -72,87 +83,81 @@ if st.sidebar.button("Predict"):
 
     predicted_species = le.inverse_transform(prediction)
 
-    st.success(
-        f"Predicted Species: {predicted_species[0]}"
-    )
+    st.success(f'Predicted Species: {predicted_species[0]}')
 
-st.subheader("Dataset Preview")
+#main body
 
+#first 5 rows
+st.subheader('Dataset Preview')
 st.dataframe(df.head())
 
-st.subheader("Dataset Information")
+#horizontal line
+st.markdown('---')
+
+#basic information about dataset
+st.subheader('Dataset Information')
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.write("Shape of Dataset")
+    st.write('Shape of Dataset')
     st.write(df.shape)
 
 with col2:
-    st.write("Missing Values")
+    st.write('Missing Values in Dataset')
     st.write(df.isnull().sum())
 
-st.subheader("Species Distribution")
+#horizontal line
+st.markdown('---')
 
-fig1, ax1 = plt.subplots(figsize=(7,5))
+#data visualization
 
-sns.countplot(
-    x="Species",
-    data=df,
-    ax=ax1
-)
+fig1, ax1 = plt.subplots(figsize = (8,8))
+sns.countplot(x = 'Species', data = df_copy, ax = ax1)
 
-st.pyplot(fig1)
+fig2, ax2 = plt.subplots(figsize = (8,8))
+corr_matrix = df_copy.drop('Species', axis = 1).corr()
+sns.heatmap(corr_matrix, annot = True, cmap = 'coolwarm', ax = ax2)
 
-st.subheader("Correlation Heatmap")
+col3, col4 = st.columns(2)
 
-fig2, ax2 = plt.subplots(figsize=(8,5))
+with col3:
+    st.subheader('Species Distribution')
+    st.pyplot(fig1)
 
-sns.heatmap(
-    df.drop("Species", axis=1).corr(),
-    annot=True,
-    cmap="coolwarm",
-    ax=ax2
-)
+with col4:
+    st.subheader('Correlation Heatmap')
+    st.pyplot(fig2)
 
-st.pyplot(fig2)
+#horizontal line
+st.markdown('---')
 
-st.subheader("Petal Length vs Petal Width")
+fig3, ax3 = plt.subplots(figsize = (8,8))
+sns.scatterplot(x = 'PetalLengthCm', y = 'PetalWidthCm',hue = 'Species', data = df_copy, ax = ax3)
 
-fig3, ax3 = plt.subplots(figsize=(7,5))
+fig4, ax4 = plt.subplots(figsize = (8,8))
+sns.boxplot(x = 'Species', y = 'SepalLengthCm', data = df_copy, ax = ax4)
 
-sns.scatterplot(
-    x="PetalLengthCm",
-    y="PetalWidthCm",
-    hue="Species",
-    data=df,
-    ax=ax3
-)
+col5, col6 = st.columns(2)
 
-st.pyplot(fig3)
+with col5:
+    st.subheader('Petal Length VS Petal Width')
+    st.pyplot(fig3)
 
-st.subheader("Sepal Length Distribution")
+with col6:
+    st.subheader('Sepal Length Distribution')
+    st.pyplot(fig4)
 
-fig4, ax4 = plt.subplots(figsize=(7,5))
+#horizontal line
+st.markdown('---')
 
-sns.boxplot(
-    x="Species",
-    y="SepalLengthCm",
-    data=df,
-    ax=ax4
-)
+st.subheader('Pair-Plot Visualization')
+pair_plot = sns.pairplot(df_copy, hue = 'Species')
+st.pyplot(pair_plot)
 
-st.pyplot(fig4)
+#horizontal line
+st.markdown('---')
 
-st.subheader("Pairplot Visualization")
-
-pairplot_fig = sns.pairplot(
-    df,
-    hue="Species"
-)
-
-st.pyplot(pairplot_fig)
-
-st.markdown("---")
-
-st.write("Made using Streamlit")
+#footer
+st.markdown('<h3 style = "text-align : center; color : #ffb6c1;">🌸 Made By Panthini Patel</h3>', unsafe_allow_html = True)
+st.markdown('<p style = "text-align : center; font-size: 20px;">🚀 Made Using Streamlit</p>', unsafe_allow_html = True)
